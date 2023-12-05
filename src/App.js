4efa0d5e1263a19e049fe1df8fe1d695a1da6ba4
bishh <yashtekavade@gmail.com>
2023-12-06 00:32:44 +0530

    css done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import './App.css';
-import ScheduleDropdown from './components/ScheduleDropdown';
-import AttendanceList from './components/AttendanceList';
-import React, { useEffect, useState } from 'react';
-import { format } from 'date-fns';
+import "./App.css";
+import ScheduleDropdown from "./components/ScheduleDropdown";
+import AttendanceList from "./components/AttendanceList";
+import React, { useEffect, useState } from "react";
+import { format } from "date-fns";
 
 function App() {
-
   const [attendanceData, setAttendanceData] = useState([]);
-  const [currentDay, setCurrentDay] = useState('');
+  const [currentDay, setCurrentDay] = useState("");
 
   useEffect(() => {
     // Fetch data from the endpoint
-    fetch('https://mern-attendance-server.vercel.app/getattendance')
+    fetch("https://mern-attendance-server.vercel.app/getattendance")
       .then((response) => response.json())
       .then((data) => setAttendanceData(data))
-      .catch((error) => console.error('Error fetching attendance data:', error));
+      .catch((error) =>
+        console.error("Error fetching attendance data:", error)
+      );
 
     // Set the current day
-    setCurrentDay(format(new Date(), 'EEEE, do MMMM'));
+    setCurrentDay(format(new Date(), "EEEE, do MMMM"));
   }, []);
 
   return (
     <div className="App">
-      <h2 className='font-bold text-4xl'>Attendance for {currentDay}</h2>
-      <ScheduleDropdown/>
+      <h2 className="font-bold text-4xl m-8">Attendance for {currentDay}</h2>
+      <ScheduleDropdown />
       <AttendanceList attendanceData={attendanceData} />
     </div>
   );
